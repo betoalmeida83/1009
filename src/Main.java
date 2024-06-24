9c54ac7af53dfa7b1355249b9e8188a3134883e0
Beto Almeida <111810182+betoalmeida83@users.noreply.github.com>
2024-06-24 11:42:27 -0300

    Update Main.java
    commit inicial falhou

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+        String nome = input.nextLine();
+        double salario = input.nextDouble();
+        double vendas = input.nextDouble();
+
+        double total = salario + (vendas * 0.15);
+
+        System.out.printf("TOTAL = R$ %.2f\n", total);
+
+        input.close();
+
     }
 }
